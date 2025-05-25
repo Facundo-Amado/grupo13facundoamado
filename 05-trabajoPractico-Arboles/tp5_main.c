f513e82cb856f6fcc868225453df0304e52df11a
Agostina Fares <agostinafares64@gmail.com>
2025-05-25 20:47:17 -0300

    ejercicio 4

--- 05-trabajoPractico-Arboles/tp5_main.c
+++ 05-trabajoPractico-Arboles/tp5_main.c
@@ -16,11 +16,108 @@
 #include "../libs/arboles/headers/arbol-binario-busqueda.h"
 
 
+/*2. Dado un árbol binario no vacío determinar:
+a. Retornar una lista con todos los nodos hojas.
+b. Retornar en una estructura todos los nodos interiores (los que no son ni 
+hojas ni raíz)
+c. Buscar todas las ocurrencias de una clave dentro del árbol. Retornar la
+posición de cada ocurrencia (puntero al nodo).
+d. Determinar la complejidad algorítmica de los puntos “a”, “b”, “c”. */
+
 //punto 2
 
 //punto 3
 
 //punto 4
+
+void cargarArbol(ArbolBinario A)
+{
+    printf("\n\t --- Carga del arbol ---\n");
+    cargar_arbol_binario(A);
+    printf("Pulse enter para volver al menu");
+    return;
+}
+
+//a
+int mainAnchoArbol(ArbolBinario A)
+{
+    Lista arbol = a_ej4_anchura(A);
+    printf("\nElementos del arbol: ");
+    l_mostrar(arbol);
+    return 0;
+}
+
+//b
+int mainCantHojas(ArbolBinario A)
+{
+    int cantHojas = a_ej4_q_hojas(A);
+    printf("\nEl arbol tiene %d hojas", cantHojas);
+    return 0;
+}
+
+//c
+int mainArbolSimilar(ArbolBinario A)
+{
+    ArbolBinario B = a_crear();
+    printf("\n\t --- Carga del arbol a comparar---\n");
+    cargar_arbol_binario(B);
+    bool res = a_ej4_similares(A, B);
+    if (res == true)
+    {
+        printf("\nLos arboles son similares");
+    }
+    else
+    {
+        printf("\nLos arboles no son similares");
+
+    }
+    return 0;
+}
+
+//d
+int mainPadreNodo(ArbolBinario A)
+{
+    int clave;
+    printf("Ingrese la clave del nodo: ");
+    while(scanf("%d", &clave) !=1)
+    {
+        printf("Dato invalido. Ingrese un numero entero: ");
+        limpiarBuffer();
+    }
+    TipoElemento x = a_ej4_padre(A, clave);
+    if (x == NULL)
+    {
+        printf("\nEl nodo no tiene padre porque es la raiz del arbol");
+    }
+    else
+    {
+        printf("\nPadre del nodo %d: %d", clave, x->clave);
+    }
+    return 0;
+}
+
+//e
+int mainHermanosNodo(ArbolBinario A)
+{
+    int clave;
+    printf("Ingrese la clave del nodo: ");
+    while(scanf("%d", &clave) !=1)
+    {
+        printf("Dato invalido. Ingrese un numero entero: ");
+        limpiarBuffer();
+    }
+    Lista hermanos = a_ej4_hermanos(A, clave);
+    if (l_es_vacia(hermanos))
+    {
+        printf("\nEl nodo no tiene hermanos");
+    }
+    else
+    {
+        printf("\nHermanos del nodo: ");
+        l_mostrar(hermanos);
+    }
+    return 0;
+}
 
 //punto 7
 
@@ -61,9 +158,10 @@ void menu_punto2()
     printf(" |                    2   Operaciones con un arbol binario                     |\n");
     printf("  ============================================================================\n");
     printf("\n");
-    printf("  1   Mostrar nodos terminales u hojas\n");
-    printf("  2   Nodos interiores\n");
-    printf("  3   Buscar todas las ocurrencias de una clave con su posición\n");
+    printf("  1   Cargar Arbol\n");
+    printf("  2   Mostrar nodos terminales u hojas\n");
+    printf("  3   Nodos interiores\n");
+    printf("  4   Buscar todas las ocurrencias de una clave con su posición\n");
     printf("\n");
     printf("  0   Salir\n");
     printf("\n");
@@ -80,11 +178,12 @@ void menu_punto4()
     printf(" |                    4   Operaciones con un arbol n-ario                    |\n");
     printf("  ============================================================================\n");
     printf("\n");
-    printf("  1   Mostrar recorrido en anchura\n");
-    printf("  2   Cantidad de hojas\n");
-    printf("  3   Arbol similar\n");
-    printf("  4   Padre del nodo\n");
-    printf("  5   Hermanos del nodo\n");
+    printf("  1   Cargar Arbol\n");
+    printf("  2   Mostrar recorrido en anchura\n");
+    printf("  3   Cantidad de hojas\n");
+    printf("  4   Arbol similar\n");
+    printf("  5   Padre del nodo\n");
+    printf("  6   Hermanos del nodo\n");
     printf("\n");
     printf("  0   Salir\n");
     printf("\n");
@@ -100,10 +199,11 @@ void menu_punto8()
     printf(" |                    8   Operaciones con un arbol n-ario                    |\n");
     printf("  ============================================================================\n");
     printf("\n");
-    printf("  1   Altura del arbol\n");
-    printf("  2   Nivel de un nodo\n");
-    printf("  3   Nodos interiores\n");
-    printf("  4   Nivel de todas las hojas\n");
+    printf("  1   Cargar Arbol\n");
+    printf("  2   Altura del arbol\n");
+    printf("  3   Nivel de un nodo\n");
+    printf("  4   Nodos interiores\n");
+    printf("  5   Nivel de todas las hojas\n");
     printf("\n");
     printf("  0   Salir\n");
     printf("\n");
@@ -114,8 +214,9 @@ void menu_punto8()
 
 int main()
 {
-    bool salir = false, salir_p2 = false, salir_p4 = false, salir_p8 = false;
+    bool salir = false, salir_p2 = false, salir_p4 = false, salir_p8 = false, arbolCargadoej2 = false, arbolCargadoej4 = false, arbolCargadoej8 = false;
     int opc, opc2, opc4, opc8;
+    ArbolBinario A2 = a_crear(), A4 = a_crear(), A8 = a_crear();
     while (!salir)
     {
         menu_principal();
@@ -132,11 +233,12 @@ int main()
         switch (opc)
         {
         case 2:
+            salir_p2 = false;
             while (!salir_p2)
             {
                 menu_punto2();
                 validador = scanf("%i", &opc2);
-                while (validador != 1 || opc2 < 0 || opc2 > 3)
+                while (validador != 1 || opc2 < 0 || opc2 > 4)
                 {
                     printf("Opción incorrecta\n");
                     printf("Seleccione una opción: ");
@@ -146,29 +248,49 @@ int main()
                 switch (opc2)
                 {
                 case 1:
-                    //main_hojas();
+                    cargarArbol(A2);
+                    arbolCargadoej2 = true;
+                    pausar();
                     break;
                 case 2:
-                    //main_interior();
+                    if(arbolCargadoej2)
+                    {
+                        //main_hojas(A2);
+                    }
+                    else printf("Primero debe cargar el arbol (opcion 1).\n");
+                    pausar();
                     break;
                 case 3:
-                    //main_buscar();
+                    if(arbolCargadoej2)
+                    {
+                        //main_interior(A2);
+                    }
+                    else printf("Primero debe cargar el arbol (opcion 1).\n");
+                    pausar();
+                    break;
+                case 4:
+                    if(arbolCargadoej2)
+                    {
+                        //main_buscar(A2);
+                    }
+                    else printf("Primero debe cargar el arbol (opcion 1).\n");                    
+                    pausar();
                     break;
                 case 0:
                     salir_p2 = true;
                 }
             }
             break;
-            menu_principal();
         case 3:
             //main_nodo_binario();
             break;
         case 4:
+            salir_p4 = false;
             while (!salir_p4)
             {
                 menu_punto4();
-                int validador = scanf("%i", &opc4);
-                while (validador != 1 || opc4 < 0 || opc4 > 5)
+                validador = scanf("%i", &opc4);
+                while (validador != 1 || opc4 < 0 || opc4 > 6)
                 {
                     printf("Opción incorrecta\n");
                     printf("Seleccione una opción: ");
@@ -178,30 +300,65 @@ int main()
                 switch (opc4)
                 {
                 case 1:
-                    mainAnchoArbol();
+                    cargarArbol(A4);
+                    arbolCargadoej4 = true;
+                    pausar();
                     break;
                 case 2:
-                    mainCantHojas();
+                    if (arbolCargadoej4)
+                    {
+                        mainAnchoArbol(A4);
+                    }
+                    else printf("Primero debe cargar el arbol (opcion 1).\n");
+                    pausar();
                     break;
                 case 3:
-                    mainArbolSimilar();
+                    if (arbolCargadoej4)
+                    {
+                        mainCantHojas(A4);
+                    }
+                    else printf("Primero debe cargar el arbol (opcion 1).\n");
+                    pausar();
                     break;
                 case 4:
-                    mainPadreNodo();
+                    if (arbolCargadoej4)
+                    {
+                        mainArbolSimilar(A4);
+                    }
+                    else printf("Primero debe cargar el arbol (opcion 1).\n");
+                    pausar();
                     break;
                 case 5:
-                    mainHermanosNodo();
+                    if (arbolCargadoej4)
+                    {
+                        mainPadreNodo(A4);
+                    }
+                    else printf("Primero debe cargar el arbol (opcion 1).\n");
+                    pausar();
                     break;
+                case 6:
+                    if (arbolCargadoej4)
+                    {
+                        mainHermanosNodo(A4);
+                    }
+                    else printf("Primero debe cargar el arbol (opcion 1).\n");
+                    pausar();
+                    break;
+                case 0:
+                    salir_p4 = true;
                 }
-                break;
-                menu_principal();
             }
+            break;
+        case 7:
+            //main_equivalentes();
+            break;
         case 8:
+            salir_p8 = false;
             while (!salir_p8)
             {
                 menu_punto8();
                 validador = scanf("%i", &opc8);
-                while (validador != 1 || opc8 < 0 || opc8 > 4)
+                while (validador != 1 || opc8 < 0 || opc8 > 5)
                 {
                     printf("Opción incorrecta\n");
                     printf("Seleccione una opción: ");
@@ -211,22 +368,46 @@ int main()
                 switch (opc8)
                 {
                 case 1:
-                    //main_hojas();
+                    cargarArbol(A8);
+                    arbolCargadoej8 = true;
+                    pausar();
                     break;
                 case 2:
-                    //main_interior();
+                    if(arbolCargadoej8)
+                    {
+                        //alturaArbol(A8);
+                    }
+                    else printf("Primero debe cargar el arbol (opcion 1).\n");
+                    pausar();
                     break;
                 case 3:
-                    //main_buscar();
+                    if(arbolCargadoej8)
+                    {
+                        //Nivel_Nodo(A8);
+                    }
+                    else printf("Primero debe cargar el arbol (opcion 1).\n");
+                    pausar();
+                    break;
+                case 4:
+                    if(arbolCargadoej8)
+                    {
+                        //Nodos_interiores(A8);
+                    }
+                    else printf("Primero debe cargar el arbol (opcion 1).\n");
+                    pausar();
+                    break;
+                case 5:
+                    if(arbolCargadoej8)
+                    {
+                        //main_nivel_hojas(A8);
+                    }
+                    else printf("Primero debe cargar el arbol (opcion 1).\n");
+                    pausar();
                     break;
                 case 0:
                     salir_p8 = true;
                 }
             }
-            break;
-            menu_principal();
-        case 7:
-            //main_equivalentes();
             break;
         case 9:
             //main_punto9();
--- 05-trabajoPractico-Arboles/tp_arboles.c
+++ 05-trabajoPractico-Arboles/tp_arboles.c
@@ -56,24 +56,141 @@ Lista a_ej3_clavesmismonivel(ArbolBinario A, int nivel){
 
 
 //punto 4
-Lista a_ej4_anchura(ArbolBinario A){
-
+//a
+Lista a_ej4_anchura(ArbolBinario A)
+{
+    Lista lista = l_crear();
+    if (a_es_vacio(A))
+    {
+        return lista;
+    }    
+    Cola c = c_crear();
+    NodoArbol actual = a_raiz(A), hijo, hermano;
+    TipoElemento te = te_crear_con_valor(0, actual);
+    c_encolar(c, te);
+    while (!c_es_vacia(c)) 
+    {
+        te = c_desencolar(c);
+        actual = (NodoArbol)te->valor;
+        l_agregar(lista, n_recuperar(actual));
+        hijo = n_hijoizquierdo(actual);
+        if (hijo != NULL) 
+        {
+            te = te_crear_con_valor(0, hijo);
+            c_encolar(c, te);
+            hermano = n_hijoderecho(hijo);
+            while (hermano != NULL) 
+            {
+                te = te_crear_con_valor(0, hermano);
+                c_encolar(c, te);
+                hermano = n_hijoderecho(hermano);
+            }
+        }
+    }
+    return lista;
 }
 
-int a_ej4_q_hojas(ArbolBinario A){
-
+//b
+int contar_hojas(NodoArbol nodo) 
+{
+    if (nodo == NULL) 
+    {
+        return 0;
+    }
+    if (n_hijoizquierdo(nodo) == NULL) 
+    {
+        return 1;
+    }
+    int hojas = 0;
+    NodoArbol hijo = n_hijoizquierdo(nodo);
+    while (hijo != NULL) 
+    {
+        hojas += contar_hojas(hijo);
+        hijo = n_hijoderecho(hijo);
+    }
+    return hojas;
 }
 
-bool a_ej4_similares(ArbolBinario A, ArbolBinario B){
-
+int a_ej4_q_hojas(ArbolBinario A)
+{
+    return contar_hojas(a_raiz(A));
 }
 
-TipoElemento a_ej4_padre(ArbolBinario A, int clave){
-
+//c
+bool son_similares(NodoArbol a, NodoArbol b) 
+{
+    if (a == NULL && b == NULL) return true;
+    if (a == NULL || b == NULL) return false;
+    return son_similares(n_hijoizquierdo(a), n_hijoizquierdo(b)) && son_similares(n_hijoderecho(a), n_hijoderecho(b));
 }
 
-Lista a_ej4_hermanos(ArbolBinario A, int clave){
+bool a_ej4_similares(ArbolBinario A, ArbolBinario B)
+{
+    return son_similares(a_raiz(A), a_raiz(B));
+}
 
+//d
+TipoElemento buscar_padre(NodoArbol nodo, int clave, NodoArbol padre) {
+    if (nodo == NULL) 
+    {
+        return NULL;
+    }
+    if (n_recuperar(nodo)->clave == clave) 
+    {
+        if (padre == NULL)  
+        {
+            return NULL;    
+        }
+        return n_recuperar(padre);
+    }
+    NodoArbol hijo = n_hijoizquierdo(nodo);
+    while (hijo != NULL) 
+    {
+        TipoElemento resultado = buscar_padre(hijo, clave, nodo);
+        if (resultado != NULL) return resultado;
+        hijo = n_hijoderecho(hijo);
+    }
+    return NULL;
+}
+
+TipoElemento a_ej4_padre(ArbolBinario A, int clave)
+{
+    return buscar_padre(a_raiz(A), clave, NULL);
+}
+
+//e
+void buscar_hermanos(NodoArbol nodo, int clave, Lista lista) 
+{
+    if (nodo == NULL) 
+    {
+        return;
+    }
+    NodoArbol hijo = n_hijoizquierdo(nodo);
+    while (hijo != NULL) 
+    {
+        if (n_recuperar(hijo)->clave == clave) 
+        {
+            NodoArbol temp = n_hijoizquierdo(nodo);
+            while (temp != NULL) 
+            {
+                if (n_recuperar(temp)->clave != clave) 
+                {
+                    l_agregar(lista, n_recuperar(temp));
+                }
+                temp = n_hijoderecho(temp);
+            }
+            return;
+        }
+        buscar_hermanos(hijo, clave, lista);
+        hijo = n_hijoderecho(hijo);
+    }
+}
+
+Lista a_ej4_hermanos(ArbolBinario A, int clave)
+{
+    Lista lista = l_crear();
+    buscar_hermanos(a_raiz(A), clave, lista);
+    return lista;
 }
 
 //punto 7
@@ -105,20 +222,31 @@ bool a_ej8_hojasmismonivel(ArbolBinario A)
 
 
 //punto 9
-//ArbolAVL a_ej9_construiravl(ArbolBinario A);
+ArbolAVL a_ej9_construiravl(ArbolBinario A){
 
-//int a_ej9_diferenciaalturas(ArbolBinario A, ArbolAVL AVL);
+}
+
+int a_ej9_diferenciaalturas(ArbolBinario A, ArbolAVL AVL){
+
+}
 
 
 //punto 10
-Lista a_ej10_generarlistaclaves(int cantidadclavesagenerar, int valorminimo, int valormaximo);
+Lista a_ej10_generarlistaclaves(int cantidadclavesagenerar, int valorminimo, int valormaximo){
 
-//ArbolBinarioBusqueda a_ej10_crearABB(Lista L);
+}
 
-//ArbolAVL a_ej10_crearAVL(Lista L);
+ArbolBinarioBusqueda a_ej10_crearABB(Lista L){
 
-//int a_ej10_difalturas(ArbolBinarioBusqueda ABB, ArbolAVL AVL);
+}
 
+ArbolAVL a_ej10_crearAVL(Lista L){
+
+}
+
+int a_ej10_difalturas(ArbolBinarioBusqueda ABB, ArbolAVL AVL){
+
+}
 
 Lista a_ej10_comparacionarboles(int N_repeticiones, int valorminimo, int valormaximo, int cantidaclavesagenerar){
 
